f354d40625cecabb64a77f79843cd84c4072870d
cmjesz <cmjesz@gmail.com>
2019-03-16 23:52:17 -0400

    Code Cleanup
    Added helper functions to clean up the code.
    Added Report button and action handler to be implimented in the future.

--- JDo/src/application/LayoutController.java
+++ JDo/src/application/LayoutController.java
@@ -6,9 +6,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.ResourceBundle;
-
-import com.sun.javafx.scene.control.skin.TitledPaneSkin;
-
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -23,8 +20,6 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 
 public class LayoutController implements Initializable {
@@ -68,6 +63,11 @@ public class LayoutController implements Initializable {
     	taskAccordion.getPanes().add(tp);
     }
     
+    @FXML
+    void generateReport(ActionEvent event) {
+    	// TODO: Add code to popup a table view and populate with data from toDoTasks
+    }
+    
     private void setupNewTitledPane(TitledPane titledPane) {
     	titledPane.setText("New Task");
     	
@@ -86,23 +86,16 @@ public class LayoutController implements Initializable {
         grid.setVgap(2);
         grid.setPadding(new Insets(5, 5, 5, 5));
         
-        Label l1 = new Label("Name:");
-        l1.setUnderline(true);
-        grid.add(l1, 0, 0);
+        grid.add(createLabel("Name:", true, "", true), 0, 0);
         
         TextField tf = new TextField();
         tf.setPromptText("Enter Task Name");
         tf.setPrefColumnCount(15);
         tf.setId("textField");
-        Label tfl = new Label();
-        tfl.setId("textFieldLabel");
-        tfl.setVisible(false);
         grid.add(tf, 0, 1);
-        grid.add(tfl, 0, 1);
+        grid.add(createLabel("", false, "textFieldLabel", false), 0, 1);
         
-        Label l2 = new Label("Notes:");
-        l2.setUnderline(true);
-        grid.add(l2, 0, 2);
+        grid.add(createLabel("Notes:", true, "", true), 0, 2);
         
         TextArea ta = new TextArea();
         ta.setPrefRowCount(2);
@@ -110,15 +103,10 @@ public class LayoutController implements Initializable {
         ta.setWrapText(true);
         ta.setPromptText("Enter Task Notes");
         ta.setId("textArea");
-        Label tal = new Label();
-        tal.setId("textAreaLabel");
-        tal.setVisible(false);
         grid.add(ta, 0, 3);
-        grid.add(tal, 0, 3);
+        grid.add(createLabel("", false, "textAreaLabel", false), 0, 3);
         
-        Label l3 = new Label("Creation:");
-        l3.setUnderline(true);
-        grid.add(l3, 0, 4);
+        grid.add(createLabel("Creation:", true, "", true), 0, 4);
         
         grid.add(new Label(dateFormat.format(date)), 0, 5);  
         
@@ -131,81 +119,16 @@ public class LayoutController implements Initializable {
         		
         		if (((Button) event.getSource()).getText().equals("Save")) {
         			ToDoTask toDoItem = toDoTasks.get(id);
-        			String title = "";
-        			String notes = "";
-        			Label textAreaLabel = null;
-        			Label textFieldLabel = null;
         			
         			GridPane gridPane = (GridPane) titledPane.getContent();
-        			for (Node node : gridPane.getChildrenUnmodifiable()) {
-        				if (node.getId() != null) {
-	        				if (node.getId().equals("textField")) {
-	        					title = ((TextField)node).getText();
-	        					((TextField)node).setVisible(false);
-	        					toDoItem.setName(title);
-	        					titledPane.setText(title);
-	        				}
-	        				else if (node.getId().equals("textArea")) {
-	        					notes = ((TextArea)node).getText();
-	        					((TextArea)node).setVisible(false);
-	        					toDoItem.setNotes(notes);
-	        				}
-	        				else if (node.getId().equals("textAreaLabel")) {
-	        					textAreaLabel = (Label) node;
-	        				}
-	        				else if (node.getId().equals("textFieldLabel")) {
-	        					textFieldLabel = (Label) node;
-	        				}
-        				}
-        			}
-        			
-        			if (textAreaLabel != null) {
-        				textAreaLabel.setText(notes);
-        				textAreaLabel.setVisible(true);
-        			}
-        			if (textFieldLabel != null) {
-        				textFieldLabel.setText(title);
-        				textFieldLabel.setVisible(true);
-        			}
+        			swapVisibleFields(gridPane, toDoItem, titledPane);
         			
         			((Button) event.getSource()).setText("Edit");
         		}
         		else {
-        			String title = "";
-        			String notes = "";
-        			TextArea textArea = null;
-        			TextField textField = null;
 
         			GridPane gridPane = (GridPane) titledPane.getContent();
-        			for (Node node : gridPane.getChildrenUnmodifiable()) {
-        				if (node.getId() != null) {
-	        				if (node.getId().equals("textField")) {
-	        					textField = (TextField) node;
-	        				}
-	        				else if (node.getId().equals("textArea")) {
-	        					textArea = (TextArea) node;
-	        				}
-	        				else if (node.getId().equals("textAreaLabel")) {
-	        					Label textAreaLabel = (Label) node;
-	        					notes = textAreaLabel.getText();
-	        					textAreaLabel.setVisible(false);
-	        				}
-	        				else if (node.getId().equals("textFieldLabel")) {
-	        					Label textFieldLabel = (Label) node;
-	        					title = textFieldLabel.getText();
-	        					textFieldLabel.setVisible(false);
-	        				}
-        				}
-        			}
-        			
-        			if (textArea != null) {
-        				textArea.setText(notes);
-        				textArea.setVisible(true);
-        			}
-        			if (textField != null) {
-        				textField.setText(title);
-        				textField.setVisible(true);
-        			}
+        			swapVisibleFields(gridPane, null, null);
         			
         			((Button) event.getSource()).setText("Save");
         		}
@@ -222,9 +145,7 @@ public class LayoutController implements Initializable {
         		
         		if (((Button) event.getSource()).getText().equals("Complete")) {
         			GridPane gp = (GridPane)((Button)event.getSource()).getParent();
-            		Label l4 = new Label("Completion:");
-                    l4.setUnderline(true);
-                    gp.add(l4, 0, 6);
+                    gp.add(createLabel("Completion:", true, "", true), 0, 6);
                     
                     DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
                     Date date = new Date();
@@ -244,5 +165,64 @@ public class LayoutController implements Initializable {
         grid.add(completeDelete, 0, 9);
         
         titledPane.setContent(grid);
+    }
+    
+    private void swapVisibleFields(GridPane gridPane, ToDoTask toDoTask, TitledPane titledPane) {
+    	Label textAreaLabel = null;
+    	Label textFieldLabel = null;
+    	TextField textField = null;
+    	TextArea textArea = null;
+    	
+    	for (Node node : gridPane.getChildrenUnmodifiable()) {
+			if (node.getId() != null) {
+				if (node.getId().equals("textField")) {
+					textField = (TextField) node;
+				}
+				else if (node.getId().equals("textArea")) {
+					textArea = (TextArea) node;
+				}
+				else if (node.getId().equals("textAreaLabel")) {
+					textAreaLabel = (Label) node;
+				}
+				else if (node.getId().equals("textFieldLabel")) {
+					textFieldLabel = (Label) node;
+				}
+			}
+		}
+    	
+    	if (textAreaLabel != null && textFieldLabel != null && textField != null && textArea != null) {
+    		if (textAreaLabel.isVisible() && textFieldLabel.isVisible()) {
+    			textField.setText(textFieldLabel.getText());
+    			textField.setVisible(true);
+    			textFieldLabel.setVisible(false);
+    			textArea.setText(textAreaLabel.getText());
+    			textArea.setVisible(true);
+    			textAreaLabel.setVisible(false);
+    		}
+    		else {
+    			String name = textField.getText();
+    			textFieldLabel.setText(name);
+    			textField.setVisible(false);
+    			textFieldLabel.setVisible(true);
+    			textAreaLabel.setText(textArea.getText());
+    			textArea.setVisible(false);
+    			textAreaLabel.setVisible(true);
+    			if (toDoTask != null) {
+	    			toDoTask.setName(name);
+	    			toDoTask.setNotes(textArea.getText());
+    			}
+    			if (titledPane != null) {
+    				titledPane.setText(name);
+    			}
+    		}
+    	}
+    }
+    
+    private Label createLabel(String text, boolean underlined, String id, boolean visible) {
+    	Label label = new Label(text);
+    	label.setUnderline(underlined);
+    	label.setId(id);
+    	label.setVisible(visible);
+    	return label;
     }
 }
